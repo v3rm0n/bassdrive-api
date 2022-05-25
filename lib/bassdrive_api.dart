a75c5dee066d27d0fd4af37b42557d6ac0ddbe97
Maido Käära <maido@producement.com>
2022-05-25 13:20:50 +0300

    Initial commit

--- lib/bassdrive_api.dart
+++ lib/bassdrive_api.dart
@@ -1,3 +1,65 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:convert';
+import 'package:collection/collection.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+
+const days = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
+];
+
+final archiveBaseUri = Uri.http('archives.bassdrivearchive.com', '/');
+
+Future<String> generateJSON() async {
+  final client = http.Client();
+  final json = {};
+  final archiveJson = {};
+  json['live'] = 'https://bassdrive.radioca.st/stream';
+  json['archive'] = archiveJson;
+  try {
+    await Future.wait(days.mapIndexed((index, day) async {
+      final dayJson = {};
+      archiveJson[day] = dayJson;
+      final showsJson = [];
+      dayJson['shows'] = showsJson;
+      final showUri = archiveBaseUri.resolve('${index + 1} - $day/');
+      final shows = await getList(client, showUri);
+      await Future.wait(shows.map((show) async {
+        final episodes = await getList(client, showUri.resolve(show));
+        if (episodes.isNotEmpty) {
+          final jsonShow = {};
+          final episodesJson = [];
+          showsJson.add(jsonShow);
+          jsonShow['episodes'] = episodesJson;
+          final showName = show.substring(0, show.length - 1);
+          jsonShow['name'] = showName;
+          await Future.wait(episodes.map((episode) async {
+            final episodeJson = {};
+            episodeJson['name'] = episode.replaceFirst('.mp3', '');
+            episodeJson['show'] = showName;
+            episodeJson['url'] = Uri.decodeFull(
+                showUri.resolve(show).resolve(episode).toString());
+            episodesJson.add(episodeJson);
+          }));
+        }
+      }));
+    }));
+  } finally {
+    client.close();
+  }
+  return jsonEncode(json);
+}
+
+Future<Iterable<String>> getList(http.Client client, Uri uri) async {
+  final response = await client.get(uri);
+  final document = parse(response.body);
+  final links = document.querySelectorAll('tr td a');
+  return links
+      .whereNot((link) => link.innerHtml == 'Parent Directory')
+      .map((link) => Uri.decodeComponent(link.attributes['href']!));
 }
--- test/bassdrive_api_test.dart
+++ test/bassdrive_api_test.dart
@@ -2,7 +2,7 @@ import 'package:bassdrive_api/bassdrive_api.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('generate', () async {
+    expect(await generateJSON(), 42);
   });
 }
