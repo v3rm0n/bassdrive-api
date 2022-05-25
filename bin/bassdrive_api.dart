760d866f69d851bbd460c0032ef143f2b9be6301
Maido Käära <maido@producement.com>
2022-05-25 12:20:05 +0300

    Initial commit

---
+++ bin/bassdrive_api.dart
@@ -0,0 +1,5 @@
+import 'package:bassdrive_api/bassdrive_api.dart' as bassdrive_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bassdrive_api.calculate()}!');
+}
---
+++ test/bassdrive_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:bassdrive_api/bassdrive_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
